7d1dd776956a3fca3c71276655a3785e1e51b489
Alperen Guncan <A.Guncan@student.tudelft.nl>
2022-12-05 17:29:03 +0100

    Modify show_plates.py

--- show_plates.py
+++ show_plates.py
@@ -18,22 +18,24 @@ if (cap.isOpened()== False):
 while(cap.isOpened()):
   # Capture frame-by-frame
   ret, frame = cap.read()
-  if ret == True:
-    detections, border = plate_detection(frame)
-    # Add predicted box
-    frame = cv2.polylines(frame, [border], True, (255, 0, 0), 3)
-    # Display the resulting frame
-    cv2.imshow('Frame', detections)
-    # Press P on keyboard to pause
-    if cv2.waitKey(24) == ord('p'):
-      while (True):
-        if cv2.waitKey(24) == ord('p'):
-          break
-    # Press Q on keyboard to exit
-    if cv2.waitKey(24) == ord('q'):
-      break
-  # Break the loop
-  else: 
+  if ret != True: break;
+  detections, border = plate_detection(frame)
+  # Add predicted box
+  frame = cv2.polylines(frame, [border], True, (255, 0, 0), 3)
+  # Display the resulting frame
+  cv2.namedWindow('Full frame', cv2.WINDOW_NORMAL)
+  cv2.imshow('Full frame', frame)
+
+  cv2.namedWindow('Cropped plate', cv2.WINDOW_NORMAL)
+  cv2.imshow('Cropped plates', detections)
+
+  # Press P on keyboard to pause
+  if cv2.waitKey(24) == ord('p'):
+    while (True):
+      if cv2.waitKey(24) == ord('p'):
+        break
+  # Press Q on keyboard to exit
+  if cv2.waitKey(24) == ord('q'):
     break
 
 # When everything done, release the video capture object
