5edcae987176846d0d89b39a96a3c611a5051b8b
Vasil Dakov <v.dakov@student.tudelft.nl>
2022-12-06 16:54:05 +0100

    Added Structure for cross-validation

--- main.py
+++ main.py
@@ -2,10 +2,17 @@ import argparse
 import os
 import CaptureFrame_Process
 import numpy as np
+from sklearn.model_selection import KFold
+import Localization
+import Recognize
+
 
 
 # define the required arguments: video path(file_path), sample frequency(second), saving path for final result table
 # for more information of 'argparse' module, see https://docs.python.org/3/library/argparse.html
+
+
+
 def get_args():
 	parser = argparse.ArgumentParser()
 	parser.add_argument('--file_path', type=str, default='./dataset/trainingsvideo.avi')
@@ -39,3 +46,29 @@ if __name__ == '__main__':
 	sample_frequency = args.sample_frequency
 	save_files = args.save_files
 	CaptureFrame_Process.CaptureFrame_Process(file_path, sample_frequency, output_path, save_files, get_hyper_args())
+
+
+def cross_validate_bounding_boxes(data, labels, hyper_args, splits=20):
+
+	kf = KFold(n_splits=splits, shuffle=True, random_state=42)
+	kf.get_n_splits(data)
+
+	for train_index, test_index in kf.split(X):
+		print("TRAIN:", train_index, "TEST:", test_index)
+		X_train, X_test = data[train_index], data[test_index]
+		y_train, y_test = labels[train_index], labels[test_index]
+
+	plates_train = Localization.plate_detection(X_train, hyper_args)[0]
+	plates_test = Localization.plate_detection(X_test, hyper_args)[0]
+
+	intersectionTrain = [value for value in plates_train if value in y_train]
+	intersectionTest = [value for value in plates_test if value in y_test]
+
+
+	print("TrainingSet:" + str(len(intersectionTrain)/len(y_train)) + "%" )
+	print("TestSet:" + str(len(intersectionTest) / len(y_test)) + "%")
+
+//def train_model()
+
+
+
