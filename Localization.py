cf4ff862e8827dd25a27c158343957709445591d
Alperen Guncan <A.Guncan@student.tudelft.nl>
2023-01-02 18:33:56 +0100

    Refactor, document, and implement multiple masking

--- Localization.py
+++ Localization.py
@@ -1,10 +1,11 @@
 import cv2
 import numpy as np
-import imutils
+from Recognize import segment_and_recognize
 
 last_image = None
 last_boxes = list()
 last_plate_imgs = list()
+desired_color_range = None
 
 """
 In this file, you need to define plate_detection function.
@@ -21,7 +22,7 @@ Hints:
 	1. You may need to define other functions, such as crop and adjust function
 	2. You may need to define two ways for localizing plates(yellow or other colors)
 """
-def plate_detection(image, hyper_args):
+def plate_detection(image, hyper_args, debug = False):
     # Contrast stretch the image
 	img = image.copy()
 
@@ -41,79 +42,104 @@ def plate_detection(image, hyper_args):
 	# ---------------------------------------
 
 	# Color segmentation
-	# Define color range
-	# Similar to lab_1_color_and_histograms color segmentation
-	colorMin = np.array(hyper_args.mask_low)
-	colorMax = np.array(hyper_args.mask_high)
-	# Segment only the selected color from the image and leave out all the rest (apply a mask)
-	mask = cv2.inRange(cv2.cvtColor(img, cv2.COLOR_BGR2HSV), colorMin, colorMax)
-	plate_imgs = cv2.bitwise_and(img, img, mask=mask)
+	# Go through every range until enough license plates are found
+	for ml, mh in zip(hyper_args.mask_low, hyper_args.mask_high):
 
-	# ---------------------------------------
-	# ------ STAGE 3 - CANNY & MORPH --------
-	# ---------------------------------------
+		global last_image, desired_color_range
+		# Guard clause for first frame
+		if last_image is None: last_image = image
+		# If new frame (not similar to last one), set new last_image and reset color range
+		if cv2.matchTemplate(image, last_image, 1) > 0.2:
+			last_image = image
+			desired_color_range = None
 
-	## Canny's algorithm for edge detection
-	edged = cv2.Canny(plate_imgs, hyper_args.canny_lower, hyper_args.canny_upper)
+		# If already have preferred color range and does not match current frame, skip iteration
+		if desired_color_range is not None and (
+			set(desired_color_range[0]) != set(ml) and set(desired_color_range[1]) != set(mh)): continue
+		
+		# Define color range
+		# Similar to Lab_1_Color_And_Histograms color segmentation
+		colorMin = np.array(ml)
+		colorMax = np.array(mh)
+		# Segment only the selected color from the image and leave out all the rest (apply a mask)
+		mask = cv2.inRange(cv2.cvtColor(img, cv2.COLOR_BGR2HSV), colorMin, colorMax)
+		plate_imgs = cv2.bitwise_and(img, img, mask=mask)
 
-	## Noise reduction after finding segmentation
-	# Using morphological filtering
-	edged_horizontal = cv2.morphologyEx(edged, cv2.MORPH_OPEN, hyper_args.opening_kernel, iterations=1)
-	edged_vertical = cv2.morphologyEx(edged, cv2.MORPH_OPEN, hyper_args.opening_kernel.T, iterations=1)
-	edged = cv2.bitwise_or(edged_horizontal, edged_vertical)
+		# ---------------------------------------
+		# ------ STAGE 3 - CANNY & MORPH --------
+		# ---------------------------------------
 
-	edged_horizontal = cv2.morphologyEx(edged, cv2.MORPH_DILATE, np.ones((1, 2)), iterations=4)
-	edged_vertical = cv2.morphologyEx(edged, cv2.MORPH_DILATE, np.ones((2, 1)), iterations=4)
-	edged = cv2.bitwise_or(edged_horizontal, edged_vertical)
+		# Using cv2's implementation directly - approved from Lab_4_Isodata_Edge_Detection
+		## Canny's algorithm for edge detection
+		edged = cv2.Canny(plate_imgs, hyper_args.canny_lower, hyper_args.canny_upper)
 
-	edged_horizontal = cv2.morphologyEx(edged, cv2.MORPH_HITMISS, hyper_args.hitmiss_kernel)
-	edged_vertical = cv2.morphologyEx(edged, cv2.MORPH_HITMISS, hyper_args.hitmiss_kernel.T)
-	edged = cv2.bitwise_or(edged_horizontal, edged_vertical)
-	cv2.imshow("Edges detected", edged)
-	
-	# ---------------------------------------
-	# --- STAGE 4 - CONTOUR / BOUNDING ------
-	# ---------------------------------------
+		# Using cv2's morph interface directly - approved from Lab_2_Morphology
+		## Noise reduction after finding segmentation
+		# Using morphological filtering
+		edged_horizontal = cv2.morphologyEx(edged, cv2.MORPH_OPEN, hyper_args.opening_kernel, iterations=1)
+		edged_vertical = cv2.morphologyEx(edged, cv2.MORPH_OPEN, hyper_args.opening_kernel.T, iterations=1)
+		edged = cv2.bitwise_or(edged_horizontal, edged_vertical)
 
-	# Find bounding boxes
-	boxes = list()
-	centers = list()
-	plate_imgs = list()
-	# By using contour detection
-	cnts, _ = cv2.findContours(edged, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-	# Check only 10 largest contours
-	cnts = sorted(cnts, key = cv2.contourArea, reverse=True)[:10]
-	for cnt in cnts:
-		# Keep perimeter for later check and approximation
-		peri = cv2.arcLength(cnt, True)
-		# Approximate polygon, given a contour, with some epsilon for roughness
-		approx = cv2.approxPolyDP(cnt, hyper_args.contour_approximation_epsilon * peri, True)
-		rect = cv2.minAreaRect(cnt)
-		ratio = max(rect[1][0], rect[1][1]) / min(rect[1][0], rect[1][1])
-		# Checked conditions for a license plate:
-		# - Check that contour approximates a quadilateral
-		# - Check that ratio of said quad is some epsilon away from 4.5, the most common license plate ratio
-		# - Check that perimeter is large enough to be considered a plate (project desc guarantees 100 plate width, hence at least 2 times that)
-		# - Check that said contour does not approximate already checked area, by comparing centers (100 pixels on x, 30 on y)
-		if len(approx) == 4 and abs(ratio - 4.5) <= hyper_args.contour_ratio_epsilon and peri > 200 and (len(centers) == 0
-			or next(filter(lambda c: abs(c[0] - rect[0][0]) > 100 or abs(c[1] - rect[0][1]) > 30, centers), None) is not None):
-			# Find bounding box endpoints and store them
-			centers.append(rect[0])
-			box = cv2.boxPoints(rect)
-			boxes.append(np.array(box).astype(np.int32))
+		edged_horizontal = cv2.morphologyEx(edged, cv2.MORPH_DILATE, np.ones((1, 2)), iterations=4)
+		edged_vertical = cv2.morphologyEx(edged, cv2.MORPH_DILATE, np.ones((2, 1)), iterations=4)
+		edged = cv2.bitwise_or(edged_horizontal, edged_vertical)
 
-			# ---------------------------------------
-			# --- STAGE 5 - WARPING / CROPPING ------
-			# ---------------------------------------
+		edged_horizontal = cv2.morphologyEx(edged, cv2.MORPH_HITMISS, hyper_args.hitmiss_kernel)
+		edged_vertical = cv2.morphologyEx(edged, cv2.MORPH_HITMISS, hyper_args.hitmiss_kernel.T)
+		edged = cv2.bitwise_or(edged_horizontal, edged_vertical)
+		if debug: cv2.imshow("Edges detected", edged)
+		
+		# ---------------------------------------
+		# --- STAGE 4 - CONTOUR / BOUNDING ------
+		# ---------------------------------------
 
+		# Find bounding boxes
+		boxes = list()
+		centers = list()
+		plate_imgs = list()
+		# By using contour detection
+		# Using cv2's contour detection implementation directly - approved from Lab_6_Find_Contours_SIFT
+		cnts, _ = cv2.findContours(edged, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+		# Check only 10 largest contours
+		cnts = sorted(cnts, key = cv2.contourArea, reverse=True)[:10]
+		for cnt in cnts:
+			# Keep perimeter for later check and approximation
+			peri = cv2.arcLength(cnt, True)
+			# Approximate polygon, given a contour, with some epsilon for roughness
+			approx = cv2.approxPolyDP(cnt, hyper_args.contour_approximation_epsilon * peri, True)
+			rect = cv2.minAreaRect(cnt)
 			# OpenCV may at times consider 90 deg rotation with flipped width/height equivalent to regular poly
-			# Hence the more complicated logic
-			rot = rect[2] if rect[2] < 45 else rect[2] - 90
-			# Rotate image to make license plate x-axis aligned
-			rot_img = cv2.warpAffine(img, cv2.getRotationMatrix2D(rect[0], rot, 1), (img.shape[1], img.shape[0]))
-			# Crop and store plate
-			rot_img = cv2.getRectSubPix(rot_img, (int(rect[1][0]), int(rect[1][1])) if rect[2] < 45 else (int(rect[1][1]), int(rect[1][0])), tuple(map(int, rect[0])))
-			plate_imgs.append(imutils.resize(rot_img, width=hyper_args.image_width))
+			ratio = rect[1][0] / rect[1][1] if rect[2] <= 45 else rect[1][1] / rect[1][0] #max(rect[1][0], rect[1][1]) / min(rect[1][0], rect[1][1])
+			# Checked conditions for a license plate:
+			# - Check that contour approximates a quadilateral
+			# - Check that ratio of said quad is some epsilon away from 4.5, the most common license plate ratio
+			# - Check that perimeter is large enough to be considered a plate (project desc guarantees 100 plate width, hence at least 2 times that)
+			# - Check that said contour does not approximate already checked area, by comparing centers (100 pixels on x, 30 on y)
+			if (
+				len(approx) == 4 and 
+				abs(ratio - 4.5) <= hyper_args.contour_ratio_epsilon and 
+				peri > 200 and 
+				(len(centers) == 0 or next(filter(lambda c: abs(c[0] - rect[0][0]) > 100 or abs(c[1] - rect[0][1]) > 30, centers), None) is not None)
+			):
+				# Find bounding box endpoints and store them
+				centers.append(rect[0])
+				box = cv2.boxPoints(rect)
+				boxes.append(np.array(box).astype(np.int32))
+
+				# ---------------------------------------
+				# --- STAGE 5 - WARPING / CROPPING ------
+				# ---------------------------------------
+
+				# OpenCV may at times consider 90 deg rotation with flipped width/height equivalent to regular poly
+				# Hence the more complicated logic
+				rot = rect[2] if rect[2] < 45 else rect[2] - 90
+				# Rotate image to make license plate x-axis aligned
+				rot_img = cv2.warpAffine(image, cv2.getRotationMatrix2D(rect[0], rot, 1), (image.shape[1], image.shape[0]))
+				# Crop and store plate
+				rot_img = cv2.getRectSubPix(rot_img, (int(rect[1][0]), int(rect[1][1])) if rect[2] < 45 else (int(rect[1][1]), int(rect[1][0])), tuple(map(int, rect[0])))
+				plate_imgs.append(cv2.resize(rot_img, hyper_args.image_dim))
+
+		if len(boxes) != 0: break
 	
 	# ---------------------------------------
 	# -------- STAGE X - DEFAULTING ---------
@@ -123,14 +149,9 @@ def plate_detection(image, hyper_args):
 	# (With of course, checking that frame has remained mostly the same)
 	if hyper_args.memoize_bounding_boxes:
 		# Default position if not able to find a bounding box on current frame
-		global last_boxes
-		global last_image
-		global last_plate_imgs
-		# Guard clause for first frame
-		if last_image is None: last_image = image
+		global last_boxes, last_plate_imgs
 		# If new frame (not similar to last one), set new last_image
 		if cv2.matchTemplate(image, last_image, 1) > 0.2:
-			last_image = image
 			last_boxes = list()
 			last_plate_imgs = list()
 		# If no plates are found, set to previous plates
@@ -144,7 +165,7 @@ def plate_detection(image, hyper_args):
 			last_plate_imgs = plate_imgs
 	return plate_imgs, boxes
 
-# Approved from lab_1_color_and_histograms
+# Approved from Lab_1_Color_And_Histograms
 def contrastImprovementContrastStretching(img, contrastFactor, minV, maxV):
 	# Guard clause
 	q = max(min(contrastFactor, 1), 0)    
@@ -155,7 +176,7 @@ def contrastImprovementContrastStretching(img, contrastFactor, minV, maxV):
 		img[:, :, c] = np.clip((img[:, :, c].astype(np.float16) - low) * (maxV - minV) / (high - low) + minV, 0.0, 255.0).astype(int)
 	return img
 
-# Approved from lab_3_filtering
+# Approved from Lab_3_Filtering
 def sharpKernel(kernel, sigma):
 	k = (kernel - 1) // 2
 	sharpen_kernel = None
