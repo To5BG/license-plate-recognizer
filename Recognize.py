cc38ee069a526724f0599e51873df1b02e61825c
Vasil Dakov <v.dakov@student.tudelft.nl>
2023-01-05 10:12:24 +0100

    Refactored segmentation to cut based on area percentage minimum and aspect ratio + normalization of recognition

--- Recognize.py
+++ Recognize.py
@@ -5,13 +5,15 @@ import os
 from Localization import sharpKernel
 
 sift = None
-references = {}
+reference_images = {}
+reference_sifts = {}
 
 """
 In this file, you will define your own segment_and_recognize function.
 To do:
     1. Segment the plates character by character
-    2. Compute the distances between character images and reference character images(in the folder of 'SameSizeLetters' and 'SameSizeNumbers')
+    2. Compute the distances between character images and reference character images(in the folder of 'SameSizeLetters' 
+    and 'SameSizeNumbers')
     3. Recognize the character by comparing the distances
 Inputs:(One)
     1. plate_imgs: cropped plate images by Localization.plate_detection function
@@ -24,8 +26,8 @@ Hints:
 """
 
 
-def segment_and_recognize(plate_imgs, hyper_args, quickCheck=False):
-    global references, sift
+def segment_and_recognize(plate_imgs, hyper_args, quick_check=False):
+    global reference_images, sift
     recognized_plates = []
 
     if sift is None:
@@ -40,7 +42,7 @@ def segment_and_recognize(plate_imgs, hyper_args, quickCheck=False):
 # Go through all files in provided filepath, and images to a in-memory dictionary
 # this can be optimized later to contain the sifts directly but it is fine for now
 def create_database(path):
-    global references, sift
+    global reference_images, reference_sifts, sift
 
     for f in os.listdir(path):
         if f.endswith('.png') or f.endswith('.jpg') or f.endswith('.bmp'):
@@ -48,9 +50,12 @@ def create_database(path):
             img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
             img = img[:, :np.max(np.where(img != 0)[1] % img.shape[1]) + 1]
             img = cv2.resize(img, (64, 80))
-            references[f.split('.')[0]] = img
+            reference_images[f.split('.')[0]] = img
+            reference_sifts[f.split('.')[0]] = sift.detectAndCompute(img, None)[1]
 
 
+# Converts the license plate into an image suitable for cutting up and xor-ing and outputs
+# the final recognition result
 def recognize_plate(image, hyper_args):
     # preprocessing steps - sharpen image and improve contour results
     img = image.copy()
@@ -59,9 +64,9 @@ def recognize_plate(image, hyper_args):
     img = cv2.bilateralFilter(img, hyper_args.bifilter_k, hyper_args.bifilter_sigma1, hyper_args.bifilter_sigma2)
     img = cv2.filter2D(img, -1, sharpKernel(hyper_args.sharpen_k, hyper_args.sharpen_sigma))
 
-    # convert to grayscale
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    # thresholded image for easier contour computation in same format as ground truth
+    # threshold image for easier contour computation in same format as ground truth - will also later be used for
+    # xor verification
     thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_OTSU + cv2.THRESH_BINARY_INV)[1]
 
     contours = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
@@ -77,7 +82,7 @@ def recognize_plate(image, hyper_args):
 
 
 def recognize_character(char):
-    global references
+    global reference_images, sift
 
     bf = cv2.BFMatcher()
     kernel = np.array([[0, -1, 0], [-1, 5, -1], [0, -1, 0]])
@@ -85,45 +90,75 @@ def recognize_character(char):
 
     k1, d1 = sift.detectAndCompute(char, None)
     allDistances = []
-    top_matches = []
+    difference_scores = []
+    images = []
 
-    for r in references:
-        k2, d2 = sift.detectAndCompute(r, None)
+    for r in reference_images:
+        k2, d2 = reference_sifts[reference_images.index(r)]
         r = cv2.resize(r, (char.shape[1], char.shape[0]))
         # Sharpen the image using a filter
         r = cv2.filter2D(r, -1, kernel)
         matches = bf.knnMatch(d1, d2, k=2)
         good_matches = []
 
+        # ratio test for sift
         for m, n in matches:
-            if m.distance < 0.9 * n.distance:
+            if m.distance < 0.8 * n.distance:
                 good_matches.append(m)
 
         distance = sum(m.distance for m in good_matches) / len(good_matches) if len(
-            good_matches) > 0 else 1000000000
-        top_matches.append(r)
+            good_matches) > 0 else 1000000000  # some arbitrarily large value if there are no sift matches at all
+        difference_scores.append(difference_score(char, r))
         allDistances.append(distance)
+        images.append(r)
 
-    for i, char in enumerate(top_matches):
-        allDistances[i] += difference_score(char, char)
+    # Normalize both lists
+    allDistances = (allDistances - np.mean(allDistances)) / np.std(allDistances)
+    difference_scores = (difference_scores - np.mean(difference_scores)) / np.std(difference_scores)
+
+    for i in range(len(allDistances)):
+        allDistances[i] += difference_scores[i]
         allDistances[i] /= 2
 
     index = np.argmin(allDistances)
-    best_image = top_matches[index]
+    best_image = images[index]
 
-    return references.index(best_image)
+    return reference_images.index(best_image)
 
 
-def segment_plate(character_contours, image, area_threshold=100):
+# function to cut up the plate into individual characters - tests by area size
+# and aspect ratio so as not to let through contours within characters
+def segment_plate(character_contours, image):
     characters = []
+    imageArea = image.shape[0] * image.shape[1]
     for c in character_contours:
-        area = cv2.contourArea(c)
-        if area > area_threshold:
-            x, y, w, h = cv2.boundingRect(c)
+        x, y, w, h = cv2.boundingRect(c)
+
+        if np.isclose(h / w, 1.75, 0.5) and w * h > 0.05 * imageArea:  # testing by area size and aspect ratio
             img = image[y:y + h, x:x + w]
             img = img[:, :np.max(np.where(img != 0)[1] % img.shape[1]) + 1]
             characters.append(img)
     return characters
+
+
+# attempt to segment based on the black spaces - so far unsuccessful
+
+# def segment_plate(image):
+#     edges = cv2.Canny(image,100,200)
+#     vertical_projection = np.sum(edges, axis=0)
+#     vertical_projection = vertical_projection != 0
+
+#     changes = np.logical_xor(vertical_projection[1:], vertical_projection[:-1])
+#     change_pts = np.nonzero(changes)[0]
+
+#     # Extract the individual character images by cropping the original image
+#     char_images = []
+#     for i in range(len(change_pts)-1):
+#         x1, y1 = 0, change_pts[i]
+#         x2, y2 = thresh.shape[1], change_pts[i+1]
+#         char_image = image[y1:y2, x1:x2]
+#         char_images.append(char_image)
+#     return char_images
 
 
 # pasted from stack overflow
@@ -143,16 +178,12 @@ def sort_contours(cnts, method="left-to-right"):
     boundingBoxes = [cv2.boundingRect(c) for c in cnts]
     (cnts, boundingBoxes) = zip(*sorted(zip(cnts, boundingBoxes), key=lambda b: b[1][i], reverse=reverse))
     # return the list of sorted contours and bounding boxes
-    return (cnts, boundingBoxes)
+    return cnts, boundingBoxes
 
 
+# xor function used as an additional verification in the recognition algorithm
 def difference_score(test_image, reference_character):
     # xor images
     arr = np.bitwise_xor(test_image, reference_character)
     # return the number of non-zero pixels
     return np.sum(arr)
-
-# def sift_descriptor(img):
-# 	global ref_sif
-# 	_, desc = ref_sif.detectAndCompute(img, None)
-# 	return np.average(desc, axis=0)
